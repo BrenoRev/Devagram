ded7b554de79d3d30ae35a4fcebfe42ae534acb7
BrenoRev <silvabreno462@gmail.com>
2022-04-26 23:58:21 -0300

    Componente de Avatar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 
 import { useState } from 'react'
+import Avatar from '../components/avatar';
 import Botao from '../components/botao';
 export default function Home() {
 
   return (
     <>
       <h1> Hello World </h1>
+      <Avatar/>
       <Botao texto={'login'} manipularClique={() => console.log('Botão clicado')} />
     </>
 

66a3b1170b1b0cf1299bf8f531f14b661cd558de
BrenoRev <silvabreno462@gmail.com>
2022-04-27 00:35:55 -0300

    Componente de upload de imagems

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
 
 import { useState } from 'react'
+import { useRef } from 'react';
 import Avatar from '../components/avatar';
 import Botao from '../components/botao';
+import { UploadImagem } from '../components/uploadImagem';
 export default function Home() {
+
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
 
   return (
     <>
       <h1> Hello World </h1>
-      <Avatar/>
-      <Botao texto={'login'} manipularClique={() => console.log('Botão clicado')} />
+
+    <button onClick={ () => referenciaInput?.current.click() }> Abrir seletor de arquivo</button>
+      
+      <UploadImagem 
+      setImagem={setImagem} 
+      imagemPreview={imagem?.preview} 
+      aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+      />
+
+      <Avatar 
+      imagem={imagem} />
+
+      <Botao 
+      texto={'login'} 
+      manipularClique={() => console.log('Botão clicado')} />
     </>
 
   )

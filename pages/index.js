79f93da96e168b1af270ec76e56bf61f118c9e15
BrenoRev <silvabreno462@gmail.com>
2022-04-28 23:46:58 -0300

    Tela de login

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,10 @@
-
 import { useState } from 'react'
 import { useRef } from 'react';
-import Avatar from '../components/avatar';
-import Botao from '../components/botao';
-import { UploadImagem } from '../components/uploadImagem';
+import Login from '../components/login';
 export default function Home() {
 
-  const [imagem, setImagem] = useState(null);
-  const referenciaInput = useRef(null);
 
   return (
-    <>
-      <h1> Hello World </h1>
-
-    <button onClick={ () => referenciaInput?.current.click() }> Abrir seletor de arquivo</button>
-      
-      <UploadImagem 
-      setImagem={setImagem} 
-      imagemPreview={imagem?.preview} 
-      aoSetarAReferencia={(ref) => referenciaInput.current = ref}
-      />
-
-      <Avatar 
-      imagem={imagem} />
-
-      <Botao 
-      texto={'login'} 
-      manipularClique={() => console.log('Botão clicado')} />
-    </>
-
+    <Login/>
   )
 }
